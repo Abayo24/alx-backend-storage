2b059c8605d95c88fce0ef0c259b3fe7bb65ee21
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-27 00:14:46 +0300

    all docs in python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,6 +6,6 @@ function that lists all documents in a collection
 
 def list_all(mongo_collection):
     """lists all documents in a collection"""
-    if db.mongo_collection.length == 0:
+    if mongo_collection.length == 0:
         return []
-    return db.mongo_collection.find()
+    return mongo_collection.find()
