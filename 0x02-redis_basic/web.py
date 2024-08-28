de54821f1182c4d97aa0473eb7d54db2a179f9b2
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-28 22:33:10 +0300

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,22 @@
+import requests
+import redis
+
+r = redis.Redis()
+
+
+def get_page(url: str) -> str:
+    """Fetches the HTML content of a URL, caches it, and tracks the access count."""
+    
+    r.incr(f"count:{url}")
+    
+    cached_page = r.get(f"cache:{url}")
+    
+    if cached_page:
+        return cached_page.decode('utf-8')
+    
+    response = requests.get(url)
+    page_content = response.text
+    
+    r.setex(f"cache:{url}", 10, page_content)
+    
+    return page_content
